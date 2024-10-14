8eabe0ef944c3211d7d8589a232b1498001c9bcc
NEVE--LESBATS Antoine <Antoine.Neve--Lesbats@bordeaux-inp.fr>
2024-10-14 09:17:24 +0200

    Debogue Position

---
+++ tec-java/DeboguePosition.java
@@ -0,0 +1,10 @@
+
+public class DeboguePosition {
+
+
+    public static void main(String[] args) {
+        Position position = new Position();
+        System.out.println("Assis :" + position.estAssis() + "\nDebout :" + position.estDebout()
+        + "\nDehors : " + position.estDehors() + "\nDedans : " + position.estInterieur());
+    }
+}
