602ee3196a313564c753ab31346d6c40282abcd0
ECK Fiona <Fiona.Eck@bordeaux-inp.fr>
2024-10-14 09:39:06 +0200

    DebogueJauge

--- tec-java/DebogueJauge.java
+++ tec-java/DebogueJauge.java
@@ -1,9 +1,13 @@
 
 public class DebogueJauge {
 
-
-    public static void main(String[] args) {
+    void testDansIntervalle(){
         Jauge jauge = new Jauge(5, 3);
         System.out.println("Vert : " + jauge.estVert() + "\nRouge : " + jauge.estRouge());
+    }
+
+    public static void main(String[] args) {
+
+        new DebogueJauge().testDansIntervalle();
     }
 }
