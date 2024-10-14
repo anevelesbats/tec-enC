c0f79d92d97c0f16f2b675c8cad65f37851152bb
anevelesbats <antoine.neve@icloud.com>
2024-10-14 08:20:06 +0200

    test class jauge

---
+++ tec-java/DebogueJauge.java
@@ -0,0 +1,9 @@
+
+public class DebogueJauge {
+
+
+    public static void main(String[] args) {
+        Jauge Jauge = new Jauge(3, 5);
+        System.out.println("Vert :" + Jauge.estVert() + "Rouge :" + Jauge.estRouge());
+    }
+}
