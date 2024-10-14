431f7da4bdc5be64711539c3bf374b56bcc98a61
NEVE--LESBATS Antoine <Antoine.Neve--Lesbats@bordeaux-inp.fr>
2024-10-14 10:03:17 +0200

    Cas de test 2

--- tec-java/DeboguePosition.java
+++ tec-java/DeboguePosition.java
@@ -4,12 +4,17 @@ public class DeboguePosition {
     void testDehorsInitial ()
     {
         Position position = new Position();
-        System.out.println("Assis :" + position.estAssis() + "\nDebout :" + position.estDebout()
+        System.out.println("Assis :" + position.estAssis() + "\nDebout :" + position.estDebout() 
         + "\nDehors : " + position.estDehors() + "\nDedans : " + position.estInterieur());
     }
 
+    void testDehorsAssis(){
+        Position position2 = new Position().assis();
+        System.out.println("Position : " + position2.estAssis());
+    }
 
     public static void main(String[] args) {
         new DeboguePosition().testDehorsInitial();
+        new DeboguePosition().testDehorsAssis();
     }
 }
