8791ba16ff0bf51ae743e0e3a46518a0fa3bbe95
anevelesbats <antoine.neve@icloud.com>
2024-10-17 09:25:09 +0200

    Suppression fichiers Jauge

--- tec-java/DebogueJauge.java
+++
@@ -1,9 +0,0 @@
-
-public class DebogueJauge {
-
-
-    public static void main(String[] args) {
-        Jauge jauge = new Jauge(5, 3);
-        System.out.println("Vert : " + jauge.estVert() + "\nRouge : " + jauge.estRouge());
-    }
-}
