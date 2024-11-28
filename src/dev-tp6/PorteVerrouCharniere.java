989cc0dac19187892431aa6e5989c91c29503aaf
DAUNY Raphael <Raphael.Dauny@bordeaux-inp.fr>
2024-11-28 08:44:56 +0100

    class porteverou

--- src/dev-tp6/PorteVerrouCharniere.java
+++ src/dev-tp6/PorteVerrouCharniere.java
@@ -1,5 +1,38 @@
 
-public class PorteVerrouCharniere implements PorteCharniere{
+public class PorteVerrouCharniere {
     private boolean estDeverouille;
+    private PorteCharniere porteCharniere;
+
+
+    public PorteVerrouCharniere() {
+        porteCharniere = new PorteCharniere(); 
+        estDeverouille = true;
+      }
+
+      public boolean estVerrouille() {
+        return estDeverouille == false;
+      }
+    
+      public void verrouiller() {
+        estDeverouille = false;
+      }
+    
+      public void deverouiller() {
+        estDeverouille = true;
+      }
+    
+      public boolean estFerme() {
+        return porteCharniere.estFerme();
+      }
+    
+      public void fermer() {
+        porteCharniere.fermer();
+      }
+    
+      public void ouvrir() {
+        if(this.estVerrouille()==false){
+          porteCharniere.ouvrir();
+        }
+      }
     
 }
