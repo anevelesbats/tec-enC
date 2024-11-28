d743c744df63b9b544bdd00b7ea9aa39a5920b36
anevelesbats <antoine.neve@icloud.com>
2024-11-28 08:44:36 +0100

    Porte Verrou Charnière

--- src/dev-tp6/PorteVerrouCharniere.java
+++ src/dev-tp6/PorteVerrouCharniere.java
@@ -1,5 +1,41 @@
 
-public class PorteVerrouCharniere implements PorteCharniere{
+public class PorteVerrouCharniere{
     private boolean estDeverouille;
+    PorteCharniere porteCharniereVerrou;
+
+    public PorteVerrouCharniere(){
+        porteCharniereVerrou = new PorteCharniere(); 
+        estDeverouille = true;
+    }
+    void verrouiller(){
+        estDeverouille = false;
+    }
+    void deverouiller(){
+        estDeverouille = true; 
+    }
+    public boolean estVerouille(){
+        return estDeverouille == false; 
+    }
+
+    public boolean estFerme() {
+        return porteCharniereVerrou.estFerme(); 
+    }
     
+    public void fermer() {
+        if (estVerouille() == false){
+            porteCharniereVerrou.fermer();
+            verrouiller();
+        }
+    }
+    
+    public void ouvrir() {
+        if (estVerouille() == false) {
+            porteCharniereVerrou.ouvrir();
+        }
+        else{
+            deverouiller();
+            porteCharniereVerrou.ouvrir(); 
+        }
+        
+    }
 }
